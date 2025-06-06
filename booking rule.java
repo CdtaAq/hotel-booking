cb60cb6d12bf263d4cedc5c0bdb44ac1be2ec6a7
chris Taylor <ctay456a@gmail.com>
2025-06-06 01:17:17 -0400

    Update booking rule.java

--- booking rule.java
+++ booking rule.java
@@ -1,9 +1,50 @@
+package com.example.booking.entity;
+
+import javax.persistence.*;
+
 @Entity
 public class BookingRule {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String ruleName;
     private String ruleDescription;
 
-    // Getters and setters
+    public BookingRule() {
+    }
+
+    public BookingRule(Long id, String ruleName, String ruleDescription) {
+        this.id = id;
+        this.ruleName = ruleName;
+        this.ruleDescription = ruleDescription;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getRuleDescription() {
+        return ruleDescription;
+    }
+
+    public void setRuleDescription(String ruleDescription) {
+        this.ruleDescription = ruleDescription;
+    }
 }
+
